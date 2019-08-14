4e6e57e421f558581efbb302bfadacf8915e76cb
02David03 <47742784+02David03@users.noreply.github.com>
2019-08-14 11:00:18 -0300

    Mudança de contador para placar

--- src/componenents/App.js
+++ src/componenents/App.js
@@ -1,11 +1,25 @@
 import React from 'react' ;
 
-import Contador from './Contador';
+import PlacarContainer from "./PlacarContainer.1";
+
+const dados = {
+    partida: {
+        estadio: "Maracanã/RJ",
+        data: "01/06/2016",
+        horario: "19h",
+    },
+    casa : {
+        nome : "Vasco",
+    },
+    visitante : {
+        nome : "Flamengo",
+    }
+};
 
 export default class App extends React.Component {
     render() {
-        return (
-                <Contador/>
-        )
+        return <PlacarContainer partida = {dados.partida}
+                                casa  = {dados.casa}
+                                visitante = {dados.visitante} />;
     }
 }
